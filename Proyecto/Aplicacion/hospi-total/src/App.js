8fa27307f58406292220763816ad4eb8b9332f58
bily98 <bilyjv98.16@gmail.com>
2019-08-10 22:13:03 -0600

    Template, Home y Ruteo agregado

--- Proyecto/Aplicacion/hospi-total/src/App.js
+++ Proyecto/Aplicacion/hospi-total/src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Sidebar from '../src/components/sidebar/sidebar';
 function App() {
-    return ( 
-        <div className="App">
-            <Sidebar></Sidebar>
-        </div>
-    );
+  return (
+    <div></div>
+  );
 }
 
 export default App;
